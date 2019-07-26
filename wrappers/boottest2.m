73d1872c8bed5afd81a68b997670311c6a3462b1
acp29 <A.C.Penn@sussex.ac.uk>
2019-07-26 20:44:12 +0100

    Update boottest2.m

--- wrappers/boottest2.m
+++ wrappers/boottest2.m
@@ -7,8 +7,8 @@
 %  Two sample bootstrap test for unpaired univariate data. The null
 %  hypothesis is that x and y are sampled from the same population.
 %  The test is two-tailed and is essentially a bootstrap version of
-%  a permutation test. Bootstrap iteration is not necessary for this 
-%  test.
+%  a permutation test. Bootstrap iteration is not implemented for 
+%  this test.
 %
 %  The syntax in this function code is known to be compatible with
 %  recent versions of Octave (v3.2.4 on Debian 6 Linux 2.6.32) and
