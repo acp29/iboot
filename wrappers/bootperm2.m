0f080a0cfffdee3037f03ca56ad809dda7908a31
acp29 <A.C.Penn@sussex.ac.uk>
2019-09-11 20:49:52 +0100

    Update bootperm2.m

--- wrappers/bootperm2.m
+++ wrappers/bootperm2.m
@@ -3,7 +3,6 @@
 %  Two-sample bootstrap (permutation) test
 %
 %   p = bootperm2(nboot,bootfun,x,y)
-%   p = bootperm2(nboot,bootfun,x,y,clusters)
 %
 %  Two sample bootstrap test for unpaired univariate data. The null
 %  hypothesis is that x and y are sampled from the same population.
@@ -11,13 +10,6 @@
 %  a permutation test. This test performs well without bootstrap
 %  iteration and so iteration is not implemented but it can only
 %  compute a P value (not a confidence interval).
-%
-%  The optional input argument, clusters must be provided as a cell
-%  array. The first cell should contain cluster definitions for x 
-%  and the second cell should contain cluster definitions for y. 
-%  An empty cell signifies that no clusters will be used in the 
-%  bootstrap for that sample. See ibootci documentation for how to 
-%  construct cluster definitions.
 %
 %  The syntax in this function code is known to be compatible with
 %  recent versions of Octave (v3.2.4 on Debian 6 Linux 2.6.32) and
@@ -42,7 +34,7 @@
 %  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-function p = bootperm2(nboot,bootfun,x,y,clusters)
+function p = bootperm2(nboot,bootfun,x,y)
 
   % Check and process ibootperm2 input arguments
   if any(size(nboot)>1)
@@ -51,44 +43,13 @@ function p = bootperm2(nboot,bootfun,x,y,clusters)
   
   % Get size of x data
   n = numel(x);  
-
-  % Evaluate optional input arguments for nested data structures
-  if nargin > 4
-    if ~iscell(clusters)
-      error('clusters must be a cell array')
-    else
-      if ~all(size(clusters) == [1 2])
-        error('clusters must be a 1-by-2 cell array')
-      end
-      if ~isempty(clusters{1})
-        if ~all(size(clusters{1}) == size(x))
-          error('dimensions of the data and clusters must be the same')
-        end
-      end
-      if ~isempty(clusters{2})
-        if ~all(size(clusters{2}) == size(y))
-          error('dimensions of the data and clusters must be the same')
-        end
-      end 
-      % Sort cluster definitions and data
-      [clusters{1},I1] = sort(clusters{1});
-      x = x(I1);
-      [clusters{2},I2] = sort(clusters{2});
-      y = y(I2);
-      % Concatenate clusters
-      clusters{2} = clusters{2} + max(clusters{1});
-      clusters = cat(1,clusters{1},clusters{2});
-    end
-  else
-    clusters = [];
-  end 
   
   % Prepare joint distribution and define function to test the null hypothesis
   z = cat(1,x,y);
   func = @(z) null(bootfun,z,n);
 
   % Use ibootci to create bootstrap statistics
-  [~,bootstat] = ibootci(nboot,{func,z},'type','per','Clusters',clusters);
+  [~,bootstat] = ibootci(nboot,{func,z},'type','per');
 
   % Calculate p-value using ibootp
   stat = func(z);
