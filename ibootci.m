eb93be975e4cc59567ef9fb255e34d8ea9df7ef7
acp29 <A.C.Penn@sussex.ac.uk>
2019-09-15 08:54:39 +0100

    Update ibootci.m

--- ibootci.m
+++ ibootci.m
@@ -183,7 +183,7 @@
 %  recent versions of Octave (v3.2.4 on Debian 6 Linux 2.6.32) and
 %  Matlab (v7.4.0 on Windows XP).
 %
-%  ibootci v2.3.4.0 (13/09/2019)
+%  ibootci v2.3.5.0 (13/09/2019)
 %  Author: Andrew Charles Penn
 %  https://www.researchgate.net/profile/Andrew_Penn/
 %
