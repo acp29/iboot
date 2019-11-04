8caf84f9005cc80286b4a4af5258d5a28e9ed7a9
Andrew Penn <A.C.Penn@sussex.ac.uk>
2019-11-04 08:38:44 +0000

    Update ibootci.m

--- ibootci.m
+++ ibootci.m
@@ -229,7 +229,7 @@
 %  recent versions of Octave (v3.2.4 on Debian 6 Linux 2.6.32) and
 %  Matlab (v7.4.0 on Windows XP).
 %
-%  ibootci v2.6.0.0 (03/11/2019)
+%  ibootci v2.6.1.0 (04/11/2019)
 %  Author: Andrew Charles Penn
 %  https://www.researchgate.net/profile/Andrew_Penn/
 %
@@ -615,7 +615,7 @@ function [ci,bootstat,S,calcurve,idx] = ibootci(argin1,argin2,varargin)
 
     % Prepare for wild bootstrap (if applicable)
     if ~isempty(residuals)
-      if ~isempty(clusters) || ~isempty(strata) || any(diff(weights))
+      if ~isempty(clusters) || ~isempty(strata) || ~isempty(blocksize) || any(diff(weights))
         error('Incompatible combination of options.')
       end
       if any(strcmpi(type,{'stud','student'}))
