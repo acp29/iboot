50a8803c1b8b1a3003a0fef27df57ac2f058a0a2
Andrew Penn <A.C.Penn@sussex.ac.uk>
2019-12-13 08:57:31 +0000

    Update ibootci.m

--- ibootci.m
+++ ibootci.m
@@ -1444,20 +1444,20 @@ end
 function T = bootclust (bootfun, K, g, runmode, mu, varargin)
 
   % Two-stage nonparametric bootstrap sampling with shrinkage
-  % correction for clustered data [1].
+  % correction for clustered data [1-4].
   %
   % By resampling residuals, this bootstrap method can be used when
   % cluster sizes are unequal. However, cluster samples are assumed
   % to be taken from populations with equal variance. Not compatible
   % with bootstrap-t or bootstrap iteration.
   %
-  % Reference:
+  % Reference5:
   %  [1] Davison and Hinkley (1997) Bootstrap Methods and their
   %       application. Chapter 3: pg 97-100
   %  [2] Ng, Grieve and Carpenter (2013) The Stata Journal.
   %       13(1): 141-164
-  %  [3] Gomes et al. (2012) Medical Decision Making.
-  %       32(2): 350-361
+  %  [3] Gomes et al. (2012) Medical Decision Making. 32(2): 350-361
+  %  [4] Gomes et al. (2012) Health Econ. 21(9):1101-18
   
   % Calculate data dimensions
   Z = varargin{1};
