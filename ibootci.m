73f4f3a62a782b06ebadf126a2d56adafed40e21
Andrew Penn <A.C.Penn@sussex.ac.uk>
2020-02-04 21:54:31 +0000

    Update ibootci.m

--- ibootci.m
+++ ibootci.m
@@ -1400,11 +1400,15 @@ function [m1, m2, S] = BC (B, func, x, T1, T0, alpha, S)
   % Calculate bias correction z0
   z0 = norminv(sum(T1<T0)/B);
 
+  % Create distribution functions 
+  stdnormcdf = @(x) 0.5*(1+erf(x/sqrt(2)));
+  stdnorminv = @(p) sqrt(2)*erfinv(2*p-1);
+  
   % Calculate Bias-corrected percentiles
-  z1 = norminv(0.5*(1+alpha));
-  m1 = normcdf(2*z0+z1);
-  z2 = norminv(0.5*(1-alpha));
-  m2 = normcdf(2*z0+z2);
+  z1 = stdnorminv(0.5*(1+alpha));
+  m1 = stdnormcdf(2*z0+z1);
+  z2 = stdnorminv(0.5*(1-alpha));
+  m2 = stdnormcdf(2*z0+z2);
   S.z0 = z0;
 
 end
