50605e7d0fa31964c24ee99ede196896510d8162
Andrew Penn <A.C.Penn@sussex.ac.uk>
2020-04-30 01:15:20 +0100

    Delete auxfun.m

--- private/auxfun.m
+++
@@ -1,9 +0,0 @@
-function T = auxfun (bootfun, nvar, varargin)
-
-  % Private function file required for ibootci
-  % Auxiliary function for block bootstrap
-  X = varargin{1};
-  Y = cat_blocks(nvar,X{:});
-  T = bootfun(Y{:});
-
-end
