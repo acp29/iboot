56c1ebcd4252265cadb7507ca3e80bc96e7396be
Andrew Penn <A.C.Penn@sussex.ac.uk>
2020-05-01 09:10:40 +0100

    Add files via upload

---
+++ private/nanfun.m
@@ -0,0 +1,16 @@
+function Y = nanfun (func, X)
+
+  % Helper function file required for ibootci
+
+  % Math functions, ignoring NaNs.
+  [m,n] = size(X);
+  if m > 1
+    Y = zeros(1,n);
+    for i = 1:n
+      Y(i) = feval(func, X(~isnan(X(:,i)),i));
+    end
+  else
+    Y = feval(func, X(~isnan(X)));
+  end
+
+end
