8e64888451073cd45a735f19b4e573c96fd34270
Andrew Penn <A.C.Penn@sussex.ac.uk>
2020-05-09 00:44:42 +0100

    Update split_blocks.m

--- helper/split_blocks.m
+++ helper/split_blocks.m
@@ -1,4 +1,4 @@
-function y = split_blocks (x, l)
+function y = split_blocks (x, l1, l2)
 
   % Helper function file required for ibootci
 
@@ -9,11 +9,18 @@ function y = split_blocks (x, l)
   % Create a matrix of circular, overlapping blocks
   % Ref: Politis and Romano (1991) Technical report No. 370
   y = cell(1,nvar);
+  l = l1;
+  i = 1;
   for v = 1:nvar
-    y{v} = zeros(n,l);
-    temp = cat(1,x{v},x{v}(1:l-1));
-    for i = 1:n
-      y{v}(i,:) = temp(i:i+l-1);
+    y{v} = zeros(n,l2);
+    while l > 0
+      temp = cat(1, x{v}(((i-1)*l1+1:min(n,i*l1)),1), ...
+                    x{v}((i-1)*l1+1:min(n,(i-1)*l1+l2),1));
+      for k = 1:l
+        y{v}((i-1)*l1+k,:) = temp(k:k+l2-1);
+      end
+      l = min(l1,n-(i*l1));
+      i = i + 1;
     end
   end
   y = cell2mat(y);
