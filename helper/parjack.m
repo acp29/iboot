9964a315895291c99c84c912350a512f1aecb66b
Andrew Penn <A.C.Penn@sussex.ac.uk>
2020-05-10 21:15:43 +0100

    Update parjack.m

--- helper/parjack.m
+++ helper/parjack.m
@@ -6,7 +6,7 @@ function T = parjack (x, func, nvar, m, i, l)
   M = cell(1,nvar);
   for v = 1:nvar
     M{v} = x{v};
-    M{v}(i:(i+l-1)) = [];
+    M{v}(i:(i+l-1),:) = [];
     M{v} = cell2mat(M{v});
   end
   T = feval(func, M{:});
